e11f3be9a27d9f635f0fa9812f52104faf6532ce
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-07 05:03:27 -0700

    Changed with cycle values

--- code.test.js
+++ code.test.js
@@ -21,9 +21,9 @@ assert.strictEqual(result, false, 'Test 2 without cycle failed');
 // Test 3 with cycle
 graph = [
     [1, 2],
-    [0, 2],
-    [0, 1, 3],
-    [2]
+    [0, 3],
+    [0, 3],
+    [1, 2]
 ];
 result = hasCycle(graph);
 assert.strictEqual(result, true, 'Test 3: Graph with a cycle failed');
