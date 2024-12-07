09f3dc1a270d1d1777f414ac15744f56c06978e8
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-06 18:45:33 -0700

    implemented hasCycle

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function hasCycle(graph) {
+    let n = graph.length;
+    let visited = new Array(n).fill(false);
+    let tracking = new Array(n).fill(false);
+
+    function searchCycle(parent, node) {
+        if(visited[node]) return false;
+        if(tracking[node]) return true;
+        visited[node] = true;
+        tracking[node] = true;
+        for(const neighbor of graph[node]) {
+            if(neighbor !== parent && searchCycle(node, neighbor)) {
+                return true;
+            }
+        }
+        tracking[node] = false;
+        return false;
+    }
+    for(let i = 0; i < n; i++) {
+        if(!visited[i] && searchCycle(-1, i)) {
+            return true;
+        }
+    }
     return false;
 }
